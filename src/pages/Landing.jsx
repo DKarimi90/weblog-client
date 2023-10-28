911ae47d79e952ddb5db37e99ee25a93175d7b08
david <davmwa90@gmail.com>
2023-10-28 19:21:56 +0300

    added BlogSlides in the Landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,16 +3,17 @@ import axios from 'axios'
 import { useBlogsContext } from '../hooks/useBlogsContext'
 import Blogs from '../components/Blogs'
 import BlogForm from '../components/BlogForm'
+import BlogSlides from '../components/BlogSlides'
 
 
-const Landing = ({url}) => {
+const Landing = () => {
   const {blogs, dispatch} = useBlogsContext()
   
   //fetch blogs to load every time component mounts
   useEffect(() => {
     const fetchBlogs = async() => {
       try {
-        const response = await axios.get(`${url}/blogs`)
+        const response = await axios.get(`http://localhost:4000/blogs`)
         dispatch({type:'SET_BLOGS', payload: response.data})
       } catch(error) {
         console.log(error)
@@ -25,7 +26,10 @@ const Landing = ({url}) => {
   
   return (
     <div className='w-full'>
-      <div className='flex'>
+      <div className='w-full h-[50vh] mb-10'>
+        <BlogSlides blogs={blogs}/>
+      </div>
+      <div className='grid md:grid-cols-2 gap-4'>
       {blogs && blogs.map((blog, index) => (
         <Blogs key={index} blog={blog}/>
       ))}
