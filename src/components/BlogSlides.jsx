0909763afa486f5281506566c181b000514fbc35
david <davmwa90@gmail.com>
2023-10-30 17:05:09 +0300

    updated the loading state for the blogs

--- src/components/BlogSlides.jsx
+++ src/components/BlogSlides.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
+import React from 'react';
 
-const BlogSlides = ( {blogs} ) => {
+const BlogSlides = ({ blogs }) => {
   return (
     <div className='w-full h-[50vh] relative'>
-      <video src="./videos/web1.mp4" alt="blog image" autoPlay muted loop className='w-full h-full object-cover'/>
+      <video src="./videos/web1.mp4" alt="blog image" autoPlay muted loop className='w-full h-full object-cover' />
       <div className='absolute bg-slate-900/70 w-full h-full top-0'></div>
       <div className='absolute top-0 left-0 right-0 bottom-0 text-center flex justify-center items-center h-full w-full text-[var(--default)] font-bold text-2xl md:text-3xl max-w-[700px] mx-auto flex-col' style={{ lineHeight: '1.5' }}>
         <h1>Discover the Magic of Storytelling at <span>BLOG<span className='text-[var(--primary)]'>BOX</span></span></h1>
         <p>A Community Where Your Stories Come to Life! Join Us and Share Your Unique Tale with the <span className='text-[var(--primary2)]'>World!</span></p>
       </div>
       <div className='flex justify-center pt-5 text-sm md:text-xl px-3'>
-        <h1>Explore <strong className='text-[var(--primary2)]'>{blogs.length}</strong> stories already shared on the platform. <span className='text-[var(--primary)]'>Enjoy the READ!</span></h1>
+        <h1>
+          {blogs ? (
+            `Explore ${blogs.length} stories already shared on the platform. Enjoy the READ!`
+          ) : (
+            'Loading...'
+          )}
+        </h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default BlogSlides
+export default BlogSlides;
