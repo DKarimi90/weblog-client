3810e6bd4ade08070f7121cfd5b5917def81319e
david <davmwa90@gmail.com>
2023-10-30 14:27:12 +0300

    updated url

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -15,7 +15,7 @@ const Landing = () => {
   useEffect(() => {
     const fetchBlogs = async() => {
       try {
-        const response = await axios.get(`http://localhost:4000/blogs`, {
+        const response = await axios.get(`https://weblog-server.onrender.com/blogs`, {
 
             headers: {
             "Content-Type": "application/json",
