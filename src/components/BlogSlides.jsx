c7ef7f4adcc8f33cab2ef565afc330a7ad6b4ce7
david <davmwa90@gmail.com>
2023-10-30 14:32:35 +0300

    updated public directory

--- src/components/BlogSlides.jsx
+++ src/components/BlogSlides.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const BlogSlides = ( {blogs} ) => {
   return (
     <div className='w-full h-[50vh] relative'>
-      <video src="./src/assets/videos/web1.mp4" alt="blog image" autoPlay muted loop className='w-full h-full object-cover'/>
+      <video src="./videos/web1.mp4" alt="blog image" autoPlay muted loop className='w-full h-full object-cover'/>
       <div className='absolute bg-slate-900/70 w-full h-full top-0'></div>
       <div className='absolute top-0 left-0 right-0 bottom-0 text-center flex justify-center items-center h-full w-full text-[var(--default)] font-bold text-2xl md:text-3xl max-w-[700px] mx-auto flex-col' style={{ lineHeight: '1.5' }}>
         <h1>Discover the Magic of Storytelling at <span>BLOG<span className='text-[var(--primary)]'>BOX</span></span></h1>
