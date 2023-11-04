1db94586a43fb91bbc8ba66a3d6bbe475eb6f8c2
david <davmwa90@gmail.com>
2023-11-04 14:29:51 +0300

    updated navbar menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,7 @@ import { useLogout } from '../hooks/useLogout'
 import { useAuthContext } from '../hooks/useAuthContext'
 import {BsSunFill, BsMoonStarsFill} from 'react-icons/bs'
 import { useEffect } from 'react'
+import {AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
 
 
 const Navbar = ( {toggleDarkMode, darkMode} ) => {
@@ -12,6 +13,7 @@ const Navbar = ( {toggleDarkMode, darkMode} ) => {
   const navigate = useNavigate()
   const [showButton, setShowButton] = useState(null)
   const [scroll, setScroll] = useState(false)
+  const [showMenu, setShowMenu] = useState(false)
 
 
   //logout function 
@@ -40,9 +42,15 @@ const Navbar = ( {toggleDarkMode, darkMode} ) => {
   }, [])
 
 
+  //mobile menu
+  const handleMenu = () => {
+    setShowMenu(!showMenu)
+  }
+
+
   return (
-    <div className={`fixed w-full flex items-center dark:bg-[var(--charcoal)] z-10 ${scroll? 'bg-slate-100/95 shadow-xl shadow-black': ''}`}>
-        {user && (<div className={`w-full h-24 max-w-[1200px] mx-auto flex justify-between items-center`}>
+    <div className={`fixed w-full flex items-center md:dark:bg-[var(--charcoal)] z-10 ${scroll? 'md:bg-slate-100/95 md:shadow-xl md:shadow-black': ''}`}>
+        {user && (<div className={`hidden w-full h-24 max-w-[1200px] mx-auto md:flex justify-between items-center px-2`}>
             <div>
                 <NavLink to='/' className='font-bold'>BLOG<span className='text-[var(--primary)]'>BOX</span></NavLink>
             </div>
@@ -56,10 +64,33 @@ const Navbar = ( {toggleDarkMode, darkMode} ) => {
               {showButton? <button className='flex justify-end w-full text-[var(--danger)] items-center' onClick={handleLogout}>Logout</button>: ''}
             </div>
           </div>)}
-          <div onClick={toggleDarkMode} className='absolute right-5 top-5'>
+          <div onClick={toggleDarkMode} className='absolute right-5 top-8 md:top-5 z-20'>
             {!darkMode? <BsSunFill />:<BsMoonStarsFill className='text-white'/>}
           </div>
-          
+
+          {/* mobile menu  */}
+          {user && <div className={`w-full md:hidden ${scroll? 'bg-[var(--default)] dark:bg-[var(--charcoal)] h-16' : ''}`}>
+            {/* humburger  */}
+            <div onClick={handleMenu} className={`absolute top-4 left-4 z-10 w-full`}>
+              {!showMenu? <AiOutlineMenu />:<AiOutlineClose />} 
+            </div>
+            
+            {/* links  */}
+            <div>
+              <div className={`flex flex-col pt-8 w-full pb-10 transform-translate duration-700 ease-in-out dark:bg-[var(--charcoal)] z-10 bg-slate-100/95 ${!showMenu? 'translate-x-[-120vh]' : 'translate-x-0'}`} >
+                  <div className='pt-6'>
+                      <NavLink onClick={handleMenu} to='/' className='font-bold px-4'>BLOG<span className='text-[var(--primary)]'>BOX</span></NavLink>
+                  </div>
+                  <div className='flex flex-col'>
+                    <NavLink onClick={handleMenu} className='mob-links' to="/resources">Resources</NavLink>
+                    <NavLink onClick={handleMenu} className='mob-links' to="/blogform">Create Blog</NavLink>
+                  </div>
+                  <div className='mob-links' onClick={handleLogout}>
+                    <NavLink>Logout</NavLink>
+                  </div>
+              </div>
+            </div>
+          </div> }
     </div>
   )
 }
