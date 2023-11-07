fbcc09d5d7f808d698558d7975eb1f8777c4d078
david <davmwa90@gmail.com>
2023-11-07 12:04:48 +0300

    added

--- src/components/BlogSlides.jsx
+++ src/components/BlogSlides.jsx
@@ -5,7 +5,7 @@ const BlogSlides = ({ blogs }) => {
     <div className='w-full h-[50vh] relative'>
       <video src="./videos/web1.mp4" alt="blog image" autoPlay muted loop className='w-full h-full object-cover' />
       <div className='absolute bg-slate-900/70 w-full h-full top-0'></div>
-      <div className='absolute top-0 left-0 right-0 bottom-0 text-center flex justify-center items-center h-full w-full text-[var(--default)] font-bold text-2xl md:text-3xl max-w-[700px] mx-auto flex-col' style={{ lineHeight: '1.5' }}>
+      <div className='absolute top-0 left-0 right-0 bottom-0 text-center flex justify-center items-center h-full w-full md:text-[var(--default)] font-bold text-lg text-[var(--primary1)] md:text-3xl max-w-[700px] mx-auto flex-col' style={{ lineHeight: '1.5' }}>
         <h1>Discover the Magic of Storytelling at <span>BLOG<span className='text-[var(--primary)]'>BOX</span></span></h1>
         <p>A Community Where Your Stories Come to Life! Join Us and Share Your Unique Tale with the <span className='text-[var(--primary2)]'>World!</span></p>
       </div>
